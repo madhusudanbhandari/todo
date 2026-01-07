3a18c87457246b3fe4d2baa0e2e3a9082d1b87b6
Madhusudan Bhandari <madhusudanb636@gmail.com>
2026-01-07 21:42:15 +0545

    login

--- flutter_application_1/lib/main.dart
+++ flutter_application_1/lib/main.dart
@@ -1,9 +1,16 @@
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_application_1/screens/firebase_options.dart';
 import 'package:flutter_application_1/screens/authscreen.dart';
 import 'package:flutter_application_1/screens/home.dart';
 
-void main() {
+// ignore: non_constant_identifier_names
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+
   runApp(const MyApp());
 }
 
