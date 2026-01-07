758205160de86e922ca408009690fbcb63ead7ca
Madhusudan Bhandari <madhusudanb636@gmail.com>
2026-01-07 13:46:15 +0545

    auth page creation

---
+++ flutter_application_1/lib/screens/authscreen.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_application_1/screens/authform.dart';
+
+class Authscreen extends StatefulWidget {
+  const Authscreen({super.key});
+
+  @override
+  State<Authscreen> createState() => _AuthscreenState();
+}
+
+class _AuthscreenState extends State<Authscreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('Login'), centerTitle: true),
+      body: const AuthForm(),
+    );
+  }
+}
